8b2f92053c7fce5d6e46540e605ca3c781c5f781
TommasoPezzola <pezzola123@gmail.com>
2023-05-02 16:19:22 +0200

    Bonus 1

--- js/main.js
+++ js/main.js
@@ -3,50 +3,58 @@ const team = [
     {
         "nome": "Wayne Barnett",
         ruolo: "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "foto": "./img/wayne-barnett-founder-ceo.jpg"
     },
 
     {
         "nome": "Angela Caroll",
         ruolo: "Chief Editor",
-        "foto": "angela-caroll-chief-editor.jpg"
+        "foto": "./img/angela-caroll-chief-editor.jpg"
     },
 
     {
         "nome": "Walter Gordon",
         ruolo: "Office Manager",
-        "foto": "walter-gordon-office-manager.jpg"
+        "foto": "./img/walter-gordon-office-manager.jpg"
     },
 
     {
         "nome": "Angela Lopez",
         ruolo: "Social Media Manager",
-        "foto": "angela-lopez-social-media-manager.jpg"
+        "foto": "./img/angela-lopez-social-media-manager.jpg"
     },
 
     {
         "nome": "Scott Estrada",
         ruolo: "Developer",
-        "foto": "scott-estrada-developer.jpg"
+        "foto": "./img/scott-estrada-developer.jpg"
     },
 
     {
         "nome": "Barbara Ramos",
         ruolo: "Graphic Designer",
-        "foto": "barbara-ramos-graphic-designer.jpg"
+        "foto": "./img/barbara-ramos-graphic-designer.jpg"
     }
 
 ];
 
 console.log(team);
 
-let main = document.getElementById("ourTeam")
+let main = document.getElementById("ourTeam");
 
 for (let i = 0; i < team.length; i++) {
-    const newDiv = document.createElement("p")
+    const newDiv = document.createElement("div")
+    const img = document.createElement("img")
     
     for (const key in team[i]) {
-        newDiv.innerText += team[i][key];
+
+        if (team[i][key].includes(".jpg")) {
+            img.src = team[i][key]
+        } else {
+            newDiv.innerText += team[i][key];
+        }
+
     }
     main.appendChild(newDiv)
+    newDiv.appendChild(img)
 }
